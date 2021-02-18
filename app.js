6663cb9beed6f736f3f797cef4855d283bc84afb
gurunoor-jft <gurunoor.singh@jellyfishtechnologies.com>
2021-02-18 18:43:53 +0530

    18 final

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ fetch(api_url,{
 method: "GET",
 }).then((db) => db.json()
 .then(data => {
+	var i=1;
 		if(data.length > 0){
 			var temp="";
 			data.forEach((u) => {
 				temp +="<tr>";
-				temp+="<td>"+u.id+"</td>";
+				temp+="<td>"+ u.id +"</td>";
 				temp+="<td>"+u.name+"</td>";
 				temp+="<td>"+u.email+"</td>";
-				temp+='<td><button onclick="deletefunction()">Delete</button></td>';
-				temp+='<td><button onclick="editfunction()">Edit</button></td>';
+				temp+='<td><button onclick="Deletefunction('+u.id+')">Delete</button></td>';
+				temp+='<td><button onclick="Editfunction()">Edit</button></td>';
 				temp+="</tr>";	
 				dataarr.push({id:u.id,name:u.name,email:u.email});
 			})
+			document.getElementById("count").innerHTML=dataarr.length;
 			document.getElementById("data").innerHTML= temp;
 		}
 	}
@@ -24,11 +26,31 @@ method: "GET",
 	console.log(error);
 });
 
+function Deletefunction(numbersssss){
+	console.log(numbersssss);
+	dataarr.splice((numbersssss-2),1);
+	
+		var temp="";
+		var i=1;
+		dataarr.forEach((u) => {
+			temp +="<tr>";
+			temp+="<td>"+ i++ +"</td>";
+			temp+="<td>"+u.name+"</td>";
+			temp+="<td>"+u.email+"</td>";
+			temp+='<td><button onclick="Deletefunction('+(i-1 )+')">Delete</button></td>';
+			temp+='<td><button onclick="Editfunction()">Edit</button></td>';
+			temp+="</tr>";	
+		})
+		document.getElementById("data").innerHTML= temp;
+	count(i);
+}
+
+
 function Addfunction(){
 	var ids=dataarr.length +1;
 	var	Name= document.getElementById('Name').value;
 	var	Email= document.getElementById('Email').value;
-	if(Name === null || Email === null || Name ==="" || Email==="")
+	if(Name === null || Email === null || Name ==="" || Email==="") 
 	{
 		alert("Enter Details");
 	}
@@ -36,15 +58,21 @@ function Addfunction(){
 	dataarr.push({id:ids,name:Name,email:Email});
 	if(dataarr.length > 0){
 		var temp="";
+		var i=1;
 		dataarr.forEach((u) => {
 			temp +="<tr>";
-			temp+="<td>"+u.id+"</td>";
+			temp+="<td>"+ i++ +"</td>";
 			temp+="<td>"+u.name+"</td>";
 			temp+="<td>"+u.email+"</td>";
-			temp+='<td><button onclick="deletefunction()">Delete</button></td>';
-			temp+='<td><button onclick="editfunction()">Edit</button></td>';
+			temp+='<td><button onclick="Deletefunction('+ (i-1) +')">Delete</button></td>';
+			temp+='<td><button onclick="Editfunction()">Edit</button></td>';
 			temp+="</tr>";	
+			
 		})
 		document.getElementById("data").innerHTML= temp;
 	}
+	count(i);
+}
+function count(total){
+	document.getElementById("count").innerHTML=total-1;
 }
