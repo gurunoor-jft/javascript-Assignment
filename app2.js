92dbacd6a9c779872583b93942eedfbd0cb8b3c5
gurunoor-jft <gurunoor.singh@jellyfishtechnologies.com>
2021-02-25 14:57:08 +0530

    Added Local Storage Final

--- app2.js
+++ app2.js
@@ -1,8 +1,26 @@
-localStorage.clear();
 var ids=[];
 var a;
-var fetched = false;
+var fetched=false;
+count(0);
 var api_url="https://jsonplaceholder.typicode.com/users";
+// if(localStorage.length === 0){
+//     load();
+// }
+// else{
+//     let temp="";
+//     for(let i = 0; i < localStorage.length; i++) {
+//         let user = JSON.parse(localStorage.getItem(`user${i}`));
+//         temp +="<tr><td>"+ parseInt(i +1) +"</td>";
+//         temp += "<td>" +user.name + "</td>";
+//         temp += "<td>" +user.email + "</td>";
+//         temp += `<td><button onClick = "store(${user.id})"style="color:red" type="button" class="fas fa-trash" data-toggle="modal" data-target="#DeleteButton"></button></td>`
+//         temp += `<td><button onClick = "editFun(${user.id})"style="color:DeepSkyBlue" type="button" class="fas fa-edit" data-toggle="modal" data-target="#EditButton"></button></td>`
+//     }
+//     count(localStorage.length);
+//     document.getElementById("data").innerHTML = temp;
+
+// }
+load();
 Display();
 async function load() {
     let res = await fetch(api_url);
@@ -16,6 +34,7 @@ async function load() {
         ids.push(u.id);
         localStorage.setItem(`user${u.id}`, JSON.stringify(obj));
     })
+    fetched=true;
     Display();
 }
 function Display(){
@@ -27,7 +46,7 @@ function Display(){
         temp += `<td>Add</td>`
         temp += `<td>Something</td></tr>`
     }
-    for(let i = 0; i < ids.length; i++) {
+   for(let i = 0; i < ids.length; i++) {
         let user = JSON.parse(localStorage.getItem(`user${ids[i]}`));
         temp +="<tr><td>"+ parseInt(i +1) +"</td>";
         temp += "<td>" +user.name + "</td>";
@@ -35,39 +54,48 @@ function Display(){
         temp += `<td><button onClick = "store(${user.id})"style="color:red" type="button" class="fas fa-trash" data-toggle="modal" data-target="#DeleteButton"></button></td>`
         temp += `<td><button onClick = "editFun(${user.id})"style="color:DeepSkyBlue" type="button" class="fas fa-edit" data-toggle="modal" data-target="#EditButton"></button></td>`
     }
+    count(ids.length);
     document.getElementById("data").innerHTML = temp;
 }
-load();
 
 function Close(){
 	document.getElementById("Name").value="";
 	document.getElementById("Email").value="";
-	document.getElementById("Name").style.borderColor = "black";
-	document.getElementById("Email").style.borderColor = "black"; 
-	document.getElementById("editName").style.borderColor = "black";
-	document.getElementById("editEmail").style.borderColor = "black"; 
+    document.getElementById("addNameSpan").innerHTML="";
+	document.getElementById("addEmailSpan").innerHTML="";
+    document.getElementById("editNameSpan").innerHTML="";
+	document.getElementById("editEmailSpan").innerHTML="";
+	document.getElementById("Name").style.border = "black 1px solid";
+	document.getElementById("Email").style.border = "black 1px solid";
+	document.getElementById("editName").style.border = "black 1px solid";
+	document.getElementById("editEmail").style.border = "black 1px solid";
 }
-function addFunction(){
+async function addFunction(){
+    if(!fetched)
+        await load();
     let newid = ids.length === 0 ? 1 : ids[ids.length-1] + 1;
 	var	Name= document.getElementById('Name').value;
 	var	Email= document.getElementById('Email').value;
 	var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 	if(Name === null || Name === "") 
 	{
-		alert("Enter Name");
-		document.getElementById("Name").style.borderColor = "red";
+        document.getElementById("addEmailSpan").innerHTML="";
+		document.getElementById("addNameSpan").innerHTML=" Add Name";
+		document.getElementById("Name").style.border = "rgb(230, 28, 28) 1px solid";
 		return false;
 	}
 	else if(Email === null || Email === ""){
-		alert("Enter Email");
-		document.getElementById("Name").style.borderColor = "black";
-		document.getElementById("Email").style.borderColor = "red";
+        document.getElementById("addNameSpan").innerHTML="";
+		document.getElementById("addEmailSpan").innerHTML=" Add Email";
+		document.getElementById("Name").style.border = "black 1px solid";
+		document.getElementById("Email").style.border = "rgb(230, 28, 28) 1px solid";
 		return false;
 	}
 	else if(!Email.match(mailformat)){
-		alert("Please Fill Email in xxx@gxxx.com format");
-		document.getElementById("Name").style.borderColor = "black";
-		document.getElementById("Email").style.borderColor = "red";
+        document.getElementById("addNameSpan").innerHTML="";
+		document.getElementById("addEmailSpan").innerHTML="Please Fill Email in xxx@gxxx.com format";
+		document.getElementById("Name").style.border = "black 1px solid";
+        document.getElementById("Email").style.border="rgb(230, 28, 28) 1px solid";
 		return false;
 	}
 	else{
@@ -78,8 +106,7 @@ function addFunction(){
     }
     localStorage.setItem(`user${newid}`, JSON.stringify(obj));
     ids.push(newid);
-    document.getElementById("Name").value="";
-	document.getElementById("Email").value="";
+    Close();
     Display();
     }
     $('#AddButton').modal('hide');
@@ -106,34 +133,38 @@ function editFunction(){
 	var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 	if(Name === null || Name === "") 
 	{
-		alert("Enter Name");
-		document.getElementById("editName").style.borderColor = "red";
+        
+		document.getElementById("editNameSpan").innerHTML = "Enter Name";
+		document.getElementById("editName").style.border = "rgb(230, 28, 28) 1px solid";
 		return false;
 	}
 	else if(Email === null || Email === ""){
-		alert("Enter Email");
-		document.getElementById("editName").style.borderColor = "black";
-		document.getElementById("editEmail").style.borderColor = "red";		
+        
+		document.getElementById("editEmailSpan").innerHTML = "Enter Email";		
+		document.getElementById("editName").style.border = "black 1px solid";
+		document.getElementById("editEmail").style.border = "rgb(230, 28, 28) 1px solid";
 		return false;
 	}
 	else if(!Email.match(mailformat)){
-		alert("Please Fill Email in xxx@gxxx.com format");
-		document.getElementById("editName").style.borderColor = "black";
-		document.getElementById("editEmail").style.borderColor = "red"; 
+
+        document.getElementById("editEmailSpan").innerHTML = "Please Fill Email in xxx@gxxx.com format";
+		document.getElementById("editName").style.border = "black 1px solid";
+		document.getElementById("editEmail").style.border = "rgb(230, 28, 28) 1px solid";
 		return false;
 	}
     else{
-        document.getElementById("editName").style.borderColor = "black";
-		document.getElementById("editEmail").style.borderColor = "black"; 
         let obj = {
             id: a, 
             name: Name,
             email: Email
         }
         localStorage.setItem(`user${a}`, JSON.stringify(obj));
+        Close();
         Display();
         $('#EditButton').modal('hide');
         return;
     }
-
+}
+function count(total){
+	document.getElementById("count").innerHTML=total;
 }
